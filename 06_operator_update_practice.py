3d5bb218839151efc873b27c7975be4d0c3f86e1
choi9525 <wjdgn9525@jbnu.ac.kr>
2026-07-15 17:48:37 +0900

    practice 연습

---
+++ 06_operator_update_practice.py
@@ -0,0 +1,14 @@
+print(3+5)
+print(3-5)
+print(3*5)
+print(6/3)
+print(3**2) #9
+print(7//2) #3
+print(7%2) # 1
+
+
+a=10, b=20, c=30
+average(a,b,c) #20
+print(average)
+print(a*b) #200
+print(a*b*c) #6000
