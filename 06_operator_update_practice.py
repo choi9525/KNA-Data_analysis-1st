577dec7e3e7a1361964a53843ef9f45e8574c88a
choi9525 <wjdgn9525@jbnu.ac.kr>
2026-07-16 09:20:59 +0900

    비교연산자

--- 06_operator_update_practice.py
+++ 06_operator_update_practice.py
@@ -12,3 +12,15 @@ average(a,b,c) #20
 print(average)
 print(a*b) #200
 print(a*b*c) #6000
+
+print("=== 비교연산자 ===")
+
+#<(미만), >(초과), <=이하, >=이상, ==같다, !=다르다
+
+print(7<16) #True
+print(7==7) #True
+print(7 != 4) #True
+
+#비교연산자는 문자열 비교도 가능하다
+
+# "안녕하세요" != "안녕하세요 "
